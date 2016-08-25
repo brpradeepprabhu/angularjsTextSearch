823d7f13803b88687e71bfeddb0984e3eb8a64d2
brpradeepprabhu <pradeep.prabhu@robosoftin.com>
2016-08-25 09:41:22 +0530

    Inital commit

---
+++ js/controller/textSeachController.js
@@ -0,0 +1,40 @@
+(function () {
+    var textSearchController = function ($http) {
+        var vm = this;
+        this.searchText = '';
+        this.http = $http;
+        $http.get('data.json').success(function (data, status, headers, config) {
+            vm.data = data;
+            vm.result = data;
+        }).error(function (data, status, headers, config) {
+            console.log("No data found..");
+        });
+    }
+
+    var p = textSearchController.prototype;
+    p.searchButtonClick = function () {
+        if (!this.searchText) {
+            this.result = this.data;
+            return;
+        }
+        var result = [];
+        var searchText = this.searchText.toLowerCase();       
+        angular.forEach(this.data, function (item) {
+
+            if (item.name.toLowerCase().indexOf(searchText) !== -1) {
+                result.push(item);
+            }
+        });
+        this.result = result
+      
+
+
+    }
+    p.resetBtnClick = function () {
+        this.result = this.data;
+        this.searchText = '';
+    }
+    angular.module("textSearchApp").controller('textSearchController', textSearchController);
+    textSearchController.$inject = ['$http'];
+}());
+
---
+++ js/filter/alphabeticalFilter.js
@@ -0,0 +1,12 @@
+angular.module("textSearchApp").filter('alphabetical', function () {
+    return function (result) {
+        if (typeof (result) !== "undefined") {
+            result.sort(function (a, b) {
+                if (a.name < b.name) return -1;
+                if (a.name > b.name) return 1;
+                return 0;
+            })
+        }
+        return result;
+    };
+});
